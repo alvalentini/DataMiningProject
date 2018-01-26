989bbfb91937e5c8c6ed6518db671bc073a5bfa0
Davide Corradini <inter.corradini@gmail.com>
2018-01-26 20:13:04 +0100

    New dataset generator and new dataset

--- code/dataset_generator.py
+++ code/dataset_generator.py
@@ -3,11 +3,14 @@ from random import randint
 
 source_dataset_size = 0
 destination_dataset_size = 500000
-destination_dataset_name = 'tweets_D.txt'
+destination_dataset_name = 'tweets_2xuser.txt'
 
 
 file_object = open('/mnt/c/Users/Davide/Desktop/tweet_text_02.csv', 'r')
 output_line = []
+
+t = {}
+tt = {}
 
 for line in file_object:
     line_list = line.split(',')
@@ -19,24 +22,38 @@ for line in file_object:
             user = line_list[i]
         if i > 1 and i < length - 4:
             tweet += line_list[i]
-    output_line.append(user + '\t\t' + tweet[1:-1])
+    if user not in t:
+        t[user] = set([])
+    t[user].add(tweet)
+    
+    if source_dataset_size % 10000 == 0:
+        print('read', source_dataset_size / 1000, 'k')
+    
+    
+    #output_line.append(user + '\t\t' + tweet[1:-1])
     source_dataset_size = source_dataset_size + 1
-    if (source_dataset_size % 100000 == 0):
-        print('Reading', int(source_dataset_size / 1000), 'K')
+    #if (source_dataset_size % 100000 == 0):
+    #    print('Reading', int(source_dataset_size / 1000), 'K')
+for k, v in t.items():
+    if len(v) > 1:
+        for tw in v:
+            output_line.append(k + '\t\t' + tw[1:-1])
 
+file = open('../data/'+ destination_dataset_name, 'w')
 
-file = open('../Dataset/'+ destination_dataset_name, 'w')
+#interval = int(source_dataset_size / destination_dataset_size)
+#print('Interval:', interval)
 
-interval = int(source_dataset_size / destination_dataset_size)
-print('Interval:', interval)
-
-for i in range(randint(0, interval), len(output_line)):
+for i in range(0, len(output_line)):
     file.write(output_line[i] + os.linesep)
-    destination_dataset_size = destination_dataset_size - 1
-    i += randint(1, interval) - 1
-    if (destination_dataset_size % 100000 == 0):
-        print('Writing', int(destination_dataset_size / 1000), 'K left')
-    if destination_dataset_size <= 0:
-        break
+
+#for i in range(randint(0, interval), len(output_line)):
+#    file.write(output_line[i] + os.linesep)
+#    destination_dataset_size = destination_dataset_size - 1
+#    i += randint(1, interval) - 1
+#    if (destination_dataset_size % 100000 == 0):
+#        print('Writing', int(destination_dataset_size / 1000), 'K left')
+#    if destination_dataset_size <= 0:
+#        break
 
 file.close()
